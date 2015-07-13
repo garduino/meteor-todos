f73f233a28e1fc887e193b8a1bf84b157f61cd8d
German Arduino <garduino@gmail.com>
2015-07-13 13:17:10 -0300

    Chaper Listing Tasks.

--- todos.js
+++ todos.js
@@ -1,6 +1,13 @@
 if(Meteor.isClient){
 // client code goes here
+
+Template.todos.helpers({ 'todo': function(){
+return Todos.find({}, {sort: {createdAt: -1}}); }
+});
+
 }
 if(Meteor.isServer){
 // server code goes here
 }
+
+Todos = new Meteor.Collection('todos');

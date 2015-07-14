cb095dae99d7eed86935377b2bf739906a177017
garduino <gsa@arsol.net>
2015-07-14 15:01:30 -0300

    Chapter Interface Tweaks.

--- todos.js
+++ todos.js
@@ -31,10 +31,13 @@ Template.todoItem.events({
     	    var confirm = window.confirm("Delete this task?"); if(confirm){
     	    	    Todos.remove({ _id: documentId });
     	    }
-}
+},
 
 'keyup [name=todoItem]': function(event){
-console.log("You just tapped a key on your keyboard.");
+var documentId = this._id;
+var todoItem = $(event.target).val();
+Todos.update({ _id: documentId }, {$set: { name: todoItem }});
+console.log("Task changed to: " + todoItem);
 }
 
 });

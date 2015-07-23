d63d0b9a3750a67fd083e14de4fc85a11a65a42b
garduino <gsa@arsol.net>
2015-07-23 08:58:08 -0300

    User Accounts - Pag 85 - 95

--- todos.js
+++ todos.js
@@ -113,6 +113,35 @@ Template.addList.events({
 		}
 });
 
+Template.register.events({
+		'submit form': function(event){
+			event.preventDefault();
+			var email = $('[name=email]').val();
+			var password = $('[name=password]').val();
+			Accounts.createUser({
+					email: email,
+					password: password
+			});
+			Router.go('home');
+		}
+});
+
+Template.navigation.events({
+		'click .logout': function(event){
+			event.preventDefault();
+			Meteor.logout();
+			Router.go('login');
+		}
+});
+
+Template.login.events({
+		'submit form': function(event){
+			event.preventDefault();
+			var email = $('[name=email]').val();
+			var password = $('[name=password]').val();
+			Meteor.loginWithPassword(email, password);
+		}
+});
 
 }
 

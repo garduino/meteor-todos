627bada6a4623139db3539db12ac329ad28327a9
German Arduino <garduino@gmail.com>
2015-07-25 10:31:37 -0300

    Error Handling part 2. Pag 100 - 101.

--- todos.js
+++ todos.js
@@ -118,13 +118,24 @@ Template.register.events({
 			event.preventDefault();
 			var email = $('[name=email]').val();
 			var password = $('[name=password]').val();
+
 			Accounts.createUser({
-					email: email,
-					password: password
-			});
-			Router.go('home');
-		}
-});
+				email: email,
+				password: password }, function(error){
+				if(error){
+				console.log(error.reason); // Output error if registration fails
+				} else {
+				Router.go("home"); // Redirect user if registration succeeds
+				} 
+				});
+			}	
+			
+				});			
+			
+			
+
+
+
 
 Template.navigation.events({
 		'click .logout': function(event){

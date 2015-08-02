b7adfa580f064e644e0545af51f825ff01126f41
German Arduino <garduino@gmail.com>
2015-08-02 09:23:57 -0300

    Showing Errors - Pag 145 - 148

--- todos.js
+++ todos.js
@@ -39,10 +39,17 @@ Template.login.onRendered(function(){
 					var email = $('[name=email]').val();
 					var password = $('[name=password]').val(); 
 					Meteor.loginWithPassword(email, password, function(error){
-							if(error){ 
-								validator.showErrors({
-										email: error.reason
-								});
+							if(error){
+								if(error.reason == "User not found"){
+									validator.showErrors({
+											email: "That email doesn't belong to a registered user."
+									}); 
+								}
+								if(error.reason == "Incorrect password"){ validator.showErrors({
+										password: "You entered an incorrect password."
+									});
+								} 
+			
 							} else {
 								var currentRoute = Router.current().route.getName(); 
 								if(currentRoute == "login"){
@@ -62,24 +69,36 @@ Template.login.onDestroyed(function(){
 		console.log("The 'login' template was just destroyed.");
 });
 
-	Template.register.onRendered(function(){
-			$('.register').validate({
-					submitHandler: function(event){
-						var email = $('[name=email]').val();
-						var password = $('[name=password]').val();
-						Accounts.createUser({
-								email: email,
-								password: password
-						}, function(error){
-							if(error){
-								console.log(error.reason);
-							} else {
-								Router.go("home");
-							}
+
+Template.register.onRendered(function(){
+		var validator = $('.register').validate({
+				submitHandler: function(event){
+					var email = $('[name=email]').val();
+					var password = $('[name=password]').val(); Accounts.createUser({
+					email: email,
+					password: password 
+					}, function(error){
+						if(error){ 
+						
+							
+					if(error){
+					if(error.reason == "Email already exists."){
+						validator.showErrors({
+						email: "That email already belongs to a registered user."
 						});
 					}
-			});
-	});
+					}
+							
+							
+						} else { 
+						Router.go("home");
+						} 
+					});
+				}
+		});
+});
+
+
 
 
 

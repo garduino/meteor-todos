0d41411158b9d6651963242349c20307b345e214
German Arduino <garduino@gmail.com>
2015-08-06 14:58:53 -0300

    Loading Progress - Pag 163 - 164

--- todos.js
+++ todos.js
@@ -1,5 +1,7 @@
+
 Router.configure({
-		layoutTemplate: 'main'
+    layoutTemplate: 'main',
+    loadingTemplate: 'loading'
 });
 
 if(Meteor.isClient){
@@ -311,48 +313,73 @@ Router.route('/login');
 Router.route('/', {
     name: 'home',
     template: 'home', 
-    subscriptions: function(){	    
-    	    var currentList = this.params._id;
-    	    return [ Meteor.subscribe('lists'), Meteor.subscribe('todos', currentList) ]	      	    
-   }
+    waitOn: function(){
+    return Meteor.subscribe('lists'); }
 });
-
-
 
 Router.route('/list/:_id', { 
 		name: 'listPage', 
 		template: 'listPage', 
 		data: function(){
-			// console.log(this.params._id);
 			var currentList = this.params._id;
 			var currentUser = Meteor.userId();
 			return Lists.findOne({ _id: currentList, createdBy: currentUser });
 		},
-		onRun: function(){
-			console.log("You triggered 'onRun' for 'listPage' route.");
-			this.next();
-		},
-		onRerun: function(){
-			console.log("You triggered 'onRerun' for 'listPage' route.");
-		},
 		onBeforeAction: function(){
 			var currentUser = Meteor.userId(); if(currentUser){
-			this.next(); 
-			} else {
-			this.render("login");
-		}
+			this.next(); } else {
+			this.render("login"); }
 		},
-		onAfterAction: function(){
-			console.log("You triggered 'onAfterAction' for 'listPage' route.");
-		},
-		onStop: function(){
-			console.log("You triggered 'onStop' for 'listPage' route.");
-		},
-		subscriptions: function(){
+		waitOn: function(){
 			var currentList = this.params._id;
-			return Meteor.subscribe('todos', currentList)
-		}
-});
+			return [ Meteor.subscribe('lists'), Meteor.subscribe('todos', currentList) ] }
+});		
+		
+//Router.route('/', {
+//    name: 'home',
+//    template: 'home', 
+//    subscriptions: function(){	    
+//    	    var currentList = this.params._id;
+//    	    return [ Meteor.subscribe('lists'), Meteor.subscribe('todos', currentList) ]	      	    
+//   }
+//});
+
+
+
+//Router.route('/list/:_id', { 
+//		name: 'listPage', 
+//		template: 'listPage', 
+//		data: function(){
+//			// console.log(this.params._id);
+//			var currentList = this.params._id;
+//			var currentUser = Meteor.userId();
+//			return Lists.findOne({ _id: currentList, createdBy: currentUser });
+//		},
+//		onRun: function(){
+//			console.log("You triggered 'onRun' for 'listPage' route.");
+//			this.next();
+//		},
+//		onRerun: function(){
+//			console.log("You triggered 'onRerun' for 'listPage' route.");
+//		},
+//		onBeforeAction: function(){
+//			var currentUser = Meteor.userId(); if(currentUser){
+//			this.next(); 
+//			} else {
+//			this.render("login");
+//		}
+//		},
+//		onAfterAction: function(){
+//			console.log("You triggered 'onAfterAction' for 'listPage' route.");
+//		},
+//		onStop: function(){
+//			console.log("You triggered 'onStop' for 'listPage' route.");
+//		},
+//		subscriptions: function(){
+//			var currentList = this.params._id;
+//			return Meteor.subscribe('todos', currentList)
+//		}
+//});
 
 
 
